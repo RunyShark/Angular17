2d724987591fb274f078c95ba6840bbf91995eaa
RunyShark <spartadragon117@live.com.mx>
2023-11-19 19:24:55 -0600

    gifs

---
+++ gifsApp/src/app/core/components/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html',
+})
+export class HomeComponent {}
