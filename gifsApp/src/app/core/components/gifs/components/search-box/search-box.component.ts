af4896ae9ea11203b0878f4d6eb53b050a25c6eb
RunyShark <spartadragon117@live.com.mx>
2023-11-19 19:51:52 -0600

    Search

--- gifsApp/src/app/core/components/gifs/components/search-box/search-box.component.ts
+++ gifsApp/src/app/core/components/gifs/components/search-box/search-box.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   styleUrl: './search-box.component.scss',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  @ViewChild('txtSearchBox')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const testInput = this.tagInput.nativeElement.value;
+    console.log('searchTag', testInput);
+  }
+}
