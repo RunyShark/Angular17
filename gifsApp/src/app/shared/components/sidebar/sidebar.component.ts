dc4fe64e2b02debb990d493965c3dd3361c8000f
RunyShark <spartadragon117@live.com.mx>
2023-11-19 19:17:58 -0600

    sidebar

---
+++ gifsApp/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent {}
