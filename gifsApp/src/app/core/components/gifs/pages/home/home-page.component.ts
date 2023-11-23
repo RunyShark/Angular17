a90de3bc576f091564b872363d52887e6f1dcddb
RunyShark <spartadragon117@live.com.mx>
2023-11-23 15:23:39 -0600

    Service

--- gifsApp/src/app/core/components/gifs/pages/home/home-page.component.ts
+++ gifsApp/src/app/core/components/gifs/pages/home/home-page.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../service/gifs.service';
+import { Gift } from '../../service/interface';
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private readonly gifsService: GifsService) {}
+
+  get gifs(): Gift[] {
+    return this.gifsService.geGifs;
+  }
+}
--- gifsApp/src/app/core/components/gifs/service/gifs.service.ts
+++ gifsApp/src/app/core/components/gifs/service/gifs.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gift, SearchResponse } from './interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private API_KEY = 'A4PmOMjUf8ljnQweISoGBz47Z5ZBIsCT';
+  private API_URL = 'https://api.giphy.com/v1/gifs';
   private _tagsHistory: string[] = [];
-  private _gifs: string[] = [];
+  private _gifs: Gift[] = [];
 
-  constructor() {}
+  constructor(private readonly http: HttpClient) {}
 
   private organizeHistory(tag: string) {
     if (!tag || tag.length === 0) return;
@@ -22,15 +26,29 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.slice(0, 10);
   }
 
+  private params(tag: string) {
+    const params = new HttpParams()
+      .set('api_key', this.API_KEY)
+      .set('limit', '10')
+      .set('q', tag);
+    return params;
+  }
+
   public get geTagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  public get geGifs(): string[] {
+  public get geGifs(): Gift[] {
     return [...this._gifs];
   }
 
   public searchTag(tag: string) {
     this.organizeHistory(tag);
+
+    this.http
+      .get<SearchResponse>(`${this.API_URL}/search`, {
+        params: this.params(tag),
+      })
+      .subscribe((res) => (this._gifs = res.data));
   }
 }
